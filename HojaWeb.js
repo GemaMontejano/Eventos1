223c590bf874938cd828cc0e34259e90e6b7df0d
GemaMontejano <119534384+GemaMontejano@users.noreply.github.com>
2023-01-31 19:32:02 +0100

    Create HojaWeb.js

---
+++ HojaWeb.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario HTML</title>
+    <link href="/js/Eventos2.css">
+    <script defer src = "/js/Eventos2.css"></script>
+</head>
+
+<body>
+
+    <h1>Formulario</h1>
+    <form method="get"></form>
+    <label for="colores">Elige un color para el contenedor:</label><br>
+	<select name="colores" id="colores"><br>
+		<option value="Rojo">Rojo</option>
+		<option value="Azul">Azul</option>
+		<option value="Amarillo">Amarillo</option>
+		<option value="Verde">Verde</option>
+	</select><br>
+	<br>
+
+    <div id="wrapper" class = "wrapper"><br><br>
+        <div id="contenedor" class="content">CONTENEDOR</div>
+   <aside class="aside" id ="extra">EXTRA</aside>
+        </div>
